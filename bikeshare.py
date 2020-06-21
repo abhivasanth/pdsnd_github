56c7cdcb400e0836954f844c5317832703855610
Abhinandan Vasanth <abhi.vasanth@gmail.com>
2020-06-20 23:11:54 -0700

    Fix conflict issue

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
-    #Inputs: City data dictionary
+    #City data dictionary for user input
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
